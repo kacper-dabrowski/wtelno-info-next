4fc23d3f183bd470d59e6d1e1690d9b824e32231
kacper-dabrowski <kdtw301@gmail.com>
2022-03-29 19:13:47 +0200

    Add breakpoints and font sizes to the theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,1 +1,33 @@
-export const theme = {};
+export const theme = {
+  breakpoints: {
+    xs: '0px',
+    sm: '600px',
+    md: '900px',
+    lg: '1200px',
+    xl: '1536px',
+  },
+  font: {
+    family: `'Lato', Fallback, sans-serif`,
+    size: {
+      paragraph: {
+        p1: '18px',
+        p2: '16px',
+        p3: '14px',
+        p4: '12px',
+      },
+      header: {
+        h1: '36px',
+        h2: '28px',
+        h3: '24px',
+        h4: '22px',
+        h5: '18px',
+      },
+    },
+    weight: {
+      extraBold: 800,
+      bold: 700,
+      semiBold: 600,
+      normal: 400,
+    },
+  },
+};
