cb5f2078610d4962c4c9cf87e296b11cdbe2322d
kacper-dabrowski <kdtw301@gmail.com>
2022-04-29 21:36:33 +0200

    Add header and iconLinks

---
+++ src/layout/header/iconLinks/iconLink/iconLink.styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const IconLink = styled.a`
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: ease-in-out 0.2s;
+  }
+`;
